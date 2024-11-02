2822917f98e194e5e8aa0eccf3064dde5cd10fa4
deepanshu724 <126802289+deepanshu724@users.noreply.github.com>
2024-11-02 15:14:07 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { ClerkProvider } from '@clerk/clerk-react'
-import { dark, shadesOfPurple } from '@clerk/themes'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { ClerkProvider } from '@clerk/clerk-react';
+import { dark } from '@clerk/themes';
 
-// Import your publishable key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+// Import environment variables
+const FRONTEND_API = import.meta.env.VITE_CLERK_FRONTEND_API;
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
+if (!PUBLISHABLE_KEY || !FRONTEND_API) {
+  throw new Error("Missing Clerk configuration keys");
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/" appearance={{
-      baseTheme:dark,
-    }}>
+    <ClerkProvider 
+      publishableKey={PUBLISHABLE_KEY} 
+      frontendApi={FRONTEND_API}
+      afterSignOutUrl="/" 
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       <App />
     </ClerkProvider>
   </React.StrictMode>,
-)
+);
